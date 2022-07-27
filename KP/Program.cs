e0d90b3fad6ef424a5aaaa9977db7f9b12edae1c
Kristina Strakh <kris.strakg@gmail.com>
2022-07-27 20:35:16 +0300

    Code

--- KP/Program.cs
+++ KP/Program.cs
@@ -1,3 +1,96 @@
 ﻿// Создание массива вручную 
+//создаем метод нахождения нужных элементов массива (БЛОК - СХЕМА), первый for 
+//перебирает первый массив и в count записывает количество элементов, то есть длину нового массива.
+//Второй цикл for перебирает первый массив и записывает подходящие элементы в новый массив. 
+//Далее создаем метод вывода через строки ( как показывали на практикуме).
+//Печать массива.
+
+string[] input =
+{"23", "лес", "каникулы", "333", "облако"};
+Console.WriteLine(PrintArray(LetterArray(input)));
+
+string[] LetterArray(string[] input)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = string.Empty;
+        temp = input[i];
+        if (temp.Length <= 3)
+        {
+            count++;
+        }
+    }
+    string[] letArray = new string[count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temporary = string.Empty;
+        temporary = input[i];
+        if (temporary.Length <= 3)
+        {
+            letArray[index] = temporary;
+            index++;
+        }
+    }
+    return letArray;
+
+
+}
+
+
+string PrintArray(string[] array)
+{
+    string letters = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        letters = letters + array[i] + " ";
+    }
+    return letters;
+
+}
+
+
+/*string[] input =
+{ "23", "лес", "каникулы", "333", "облако"};
+Console.WriteLine(PrintArray(LetterArray(input)));
+
+string[] LetterArray(string[] input)
+{
+    int count = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = string.Empty;
+        temp = input[i];
+        if (temp.Length <= 3)
+        {
+            count++; // сколько слов 3 слова 
+        }
+    }
+    string[] letArray = new string[count]; // перебираем первый массив записываем в новый массив 
+    int index = 0;
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = string.Empty;
+        temp = input[i];
+        if (temp.Length <= 3)
+        {
+            letArray[index] = temp;
+            index++;
+        }
+    }
+    return letArray;
+
+}
+string PrintArray(string[] array)
+{
+    string output = string.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+        output = output + array[i] + " "; //пустая строка выбираем массив - входит то, что выходит - выводим
+    }
+    return output;
+}*/
 
 
